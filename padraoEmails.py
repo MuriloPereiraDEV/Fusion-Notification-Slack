32c78f8bbe1893a7205a8a234e460e5115056a70
MuriloPereiraDEV <102379940+MuriloPereiraDEV@users.noreply.github.com>
2022-10-21 17:55:17 -0300

    Delete padraoEmails.py

--- padraoEmails.py
+++
@@ -1,9 +0,0 @@
-#VARIAVEIS QUE ARMAZENAM PADRÕES DE E-MAILS QUE O FUSION PODE DISPARAR
-devolutivaSolicitaçãoPagamento = "Devolutiva da Solicitação de Pagamento"
-neomindFusionTarefaAvisoPagamento = "Neomind Fusion - Tarefa em Aviso - Pagamento"
-neomindFusionTarefaAtrasadaPagamento = "Neomind Fusion - Tarefa Atrasada - Pagamento"
-novaTarefaDisponívelExecução = "Nova tarefa disponível para execução"
-tarefaPendente = "Tarefa Pendente"
-tarefaRealocada = "Tarefa Realocada"
-tarefaAtrasada = "Tarefa Atrasada"
-tarefaAviso = "Tarefa em Aviso"
