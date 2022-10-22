89aa095e39c82ad37c694a4a8727922d8d1eeafc
MuriloPereiraDEV <102379940+MuriloPereiraDEV@users.noreply.github.com>
2022-10-21 22:57:02 -0300

    Change Comments

--- dadosFuncionariosSlack.py
+++ dadosFuncionariosSlack.py
@@ -1,8 +1,8 @@
-#LEITURA DO ARQUIVO CSV COM OS MEMBROS DO SLACK E CRIAÇÃO DO DICIONÁRIO
 import csv
 
 dados = {}
 
+#READING THE CSV FILE WITH SLACK MEMBERS AND CREATION OF THE DICTIONARY
 with open('G:/Meu Drive/Softwares/botDicionario/csvFuncionarios/slack-ssoftex-members.csv', mode='r') as arquivo:
     arquivoFormatado = csv.reader(arquivo)
     dados = {rows[0]:rows[1] for rows in arquivoFormatado}
